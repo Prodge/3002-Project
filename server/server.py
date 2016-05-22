d1b611094f2083b83ee55e33f2ef50c4539e4bb7
prodge <prodge@prodge.net>
2016-05-22 23:46:30 +0800

    Added db init to server

--- server/server.py
+++ server/server.py
@@ -2,27 +2,29 @@ import socket
 import ssl
 
 from settings import *
-from logger import log_in_out, log
+from logger import *
+import database as db
 
 @log_in_out
-def init_socks():
+def init():
     '''
-    Initialises the server sockets
+    Initialises the server sockets and database
     '''
+    db.init()
     soc = socket.socket()
     soc.bind(('', PORT))
     soc.listen(MAX_CONNECTIONS)
     ssl_soc = ssl.wrap_socket(
         soc,
-        ssl_version=ssl.PROTOCOL_TLSv1,
-        cert_reqs=ssl.CERT_NONE,  # not sure if this is important
-        server_side=True,
-        keyfile='./cert.pem',
-        certfile='./cert.pem')
+        ssl_version = ssl.PROTOCOL_TLSv1,
+        cert_reqs = ssl.CERT_NONE,  # not sure if this is important
+        server_side = True,
+        keyfile = KEY_FILE,
+        certfile = CERT_FILE)
     return ssl_soc
 
 def main():
-    soc = init_socks()
+    soc = init()
     while True:
         conn, address = soc.accept()
         print 'got connection from {}'.format(address)
