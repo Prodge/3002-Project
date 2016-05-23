949449a41496d69be3aa591902ca282f2141cd43
prodge <prodge@prodge.net>
2016-05-23 22:39:05 +0800

    Added generic function for writing files to file system from socket. Also added cert upload task

--- server/tasks.py
+++ server/tasks.py
@@ -1,11 +1,20 @@
 from settings import *
 from logger import *
 
+def write_file_from_socket(folder, filename, conn):
+    f = open('{}/{}'.format(folder, filename), 'wb')
+    chunk = conn.recv(MAX_BUFFER_SIZE)
+    while (chunk):
+        f.write(chunk)
+        conn.recv(MAX_BUFFER_SIZE)
+    f.close()
+
+
 @log_in_out
 def task_add(data, conn):
     filename = data.get('filename', None)
     if not filename:
-        raise ValueError('No filename recieved')
+        raise ValueError('No filename received')
 
     if not is_file_in_database(filename):
         add_file_cert_mapping(filename, '')
@@ -15,12 +24,7 @@ def task_add(data, conn):
     if file_exists(filename):
         remove_file(filename)
 
-    f = open('{}/{}'.format(FILES_FOLDER, filename), 'wb')
-    chunk = conn.recv(MAX_BUFFER_SIZE)
-    while (chunk):
-        f.write(chunk)
-        conn.recv(MAX_BUFFER_SIZE)
-    f.close()
+    write_file_from_socket(FILES_FOLDER, filename, conn)
 
 @log_in_out
 def task_list(data, conn):
@@ -29,3 +33,16 @@ def task_list(data, conn):
             for mapping in get_file_cert_mappings()
     ]
     conn.send(json.dumps(mappings_dict))
+
+@log_in_out
+def task_cert(data, conn):
+    filename = data.get('filename', None)
+    if not filename:
+        raise ValueError('No filename received')
+
+    conn.send('ready to receive')
+
+    if file_exists(filename):
+        remove_file(filename)
+
+    write_file_from_socket(CERTS_FOLDER, filename, conn)
