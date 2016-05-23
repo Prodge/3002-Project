c5046068de309d09811325362975e6155b3e5559
prodge <prodge@prodge.net>
2016-05-23 22:20:26 +0800

    Moved queries on filesystem and database to seperate file

--- server/tasks.py
+++ server/tasks.py
@@ -1,54 +1,5 @@
-from os.path import exists
-from os import remove, makedirs
-
 from settings import *
 from logger import *
-from database import query
-
-def is_file_in_database(filename):
-    res = query(
-        '''
-        select count(*)
-        from {}
-        where filename = "{}"
-        '''.format(DB_TABLENAME_FILES, filename)
-    )
-    return res[0] != 0
-
-def add_file_cert_mapping(filename, certname):
-    query(
-        '''
-        insert into {}
-        values ("{}", "{}")
-        '''.format(DB_TABLENAME_FILES, filename, certname)
-    )
-
-def update_file_cert_mapping(filename, new_certname):
-    query(
-        '''
-        update {}
-        set certname = "{}"
-        where filename = "{}"
-        '''.format(DB_TABLENAME_FILES, new_certname, filename)
-    )
-
-def get_file_cert_mappings():
-    return query('select * from {}'.format(DB_TABLENAME_FILES))
-
-def get_file_cert_mapping(filename):
-    return query('select * from {} where filename="{}"'.format(DB_TABLENAME_FILES, filename))
-
-def file_exists(filename):
-    return exists('{}/{}'.format(FILES_FOLDER, filename))
-
-def cert_exists(certname):
-    return exists('{}/{}'.format(CERTS_FOLDER, certname))
-
-def remove_file(filename):
-    remove('{}/{}'.format(FILES_FOLDER, filename))
-
-def remove_cert(filename):
-    remove('{}/{}'.format(CERTS_FOLDER, certname))
 
 @log_in_out
 def task_add(data, conn):
