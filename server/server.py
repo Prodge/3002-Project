8397eeee45b692eb830c0a0da11677c38354089b
prodge <prodge@prodge.net>
2016-05-28 16:21:01 +0800

    Added argument parsing

--- server/server.py
+++ server/server.py
@@ -9,6 +9,7 @@ import ssl
 from settings import *
 from logger import *
 import database as db
+import settings
 import tasks
 
 @log_in_out
@@ -40,7 +41,56 @@ def init():
         certfile = CERT_FILE)
     return ssl_soc
 
+@log_in_out
+def parse_options():
+    parser = OptionParser(usage="Usage: python server.py [options]")
+    parser.add_option(
+        "-l", "--logging-enabled",
+        action = "store_true",
+        default = LOGGING_ENABLED
+    )
+    parser.add_option(
+        "-o", "--log-file",
+        action = "store",
+        default = LOG_FILE
+    )
+    parser.add_option(
+        "-c", "--certs-folder",
+        action = "store",
+        default = CERTS_FOLDER
+    )
+    parser.add_option(
+        "-f", "--files-folder",
+        action = "store",
+        default = FILES_FOLDER
+    )
+    parser.add_option(
+        "-a", "--cert-file",
+        action = "store",
+        default = CERT_FILE
+    )
+    parser.add_option(
+        "-k", "--key-file",
+        action = "store",
+        default = KEY_FILE
+    )
+    parser.add_option(
+        "-p", "--port",
+        action = "store",
+        default = PORT
+    )
+    parser.add_option(
+        "-d", "--db-filename",
+        action = "store",
+        default = DB_FILENAME
+    )
+    options, _ = parser.parse_args()
+
+    for option, value in options.itteritems():
+        setattr(settings, option.upper(), value)
+
 def main():
+    parse_options()
     soc = init()
     while True:
         conn, address = soc.accept()
